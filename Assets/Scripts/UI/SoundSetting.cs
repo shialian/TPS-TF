7a976813c5ec3e7dd6015cf4fbe0cfbcbed54a25
shialian <50765318+shialian@users.noreply.github.com>
2023-03-05 10:36:40 +0800

    Bugs Fixed
    1. 音量調整介面移除戰鬥機音效

--- Assets/Scripts/UI/SoundSetting.cs
+++ Assets/Scripts/UI/SoundSetting.cs
@@ -7,23 +7,19 @@ public class SoundSetting : MonoBehaviour
 {
     public Slider bgm;
     public Slider sound;
-    public Slider fighterSound;
     public GameObject startMenu;
     public GameObject pauseMenu;
-    public AudioSource fighterAudio;
 
     private void Start()
     {
         bgm.value = AudioManager.singleton.source.volume;
         sound.value = SoundManager.singleton.source.volume;
-        fighterSound.value = fighterAudio.volume;
     }
 
     private void Update()
     {
         AudioManager.singleton.source.volume = bgm.value;
         SoundManager.singleton.source.volume = sound.value;
-        fighterAudio.volume = fighterSound.value;
     }
 
     public void Prev()
